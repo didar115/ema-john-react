b43b67eeb2250a01d2cd05649ce638ffa7958714
didar <dider.alam115@gmail.com>
2021-09-28 22:34:25 +0600

    cart functionality done :heavy_check_mark:

--- src/componets/Cart/Cart.jsx
+++ src/componets/Cart/Cart.jsx
@@ -8,14 +8,19 @@ function Cart(props) {
     for (const product of cart) {
         total = total + product.price;
     }
+
+    const shipping = total > 0 ? 15 : 0;
+    const tax = (total + shipping) * 0.10;
+    const grandTotal = total + shipping + tax;
+
     return (
 			<div>
 				<h3>Order Summary</h3>
                 <h5>Item Odered: { props.cart.length} </h5>
-                <p>Items: </p>
-                <p>Total: {total}</p>
-                <p>Shipping & Handing: </p>
-				
+                <h5>Total: {total.toFixed(2)}</h5>
+                <h5>Shipping & Handing: { shipping.toFixed(2)}</h5>
+                <h5>Tax: {tax.toFixed(2)}</h5>
+                <h5>Grand Total: { grandTotal.toFixed(2)}</h5>
 			</div>
 		);
 }
--- src/componets/Product/Product.jsx
+++ src/componets/Product/Product.jsx
@@ -17,7 +17,7 @@ function Product(props) {
                 <p> Price: {price}</p>
                 <p><small>Only {stock} left in stock - order soon</small></p>
                 
-                <button className='regular-btn' onClick={()=>props.handleAddToCart(props.Product)}>add to cart</button>
+                <button className='regular-btn' onClick={()=>props.handleAddToCart(props.Product)}> <i className="fas fa-shopping-cart"></i> add to cart</button>
 			</div>
 		</div>
 	);
