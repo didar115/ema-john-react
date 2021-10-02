9929e9e0532429feee7d42babeb0baa1286e9249
didar <dider.alam115@gmail.com>
2021-10-02 22:11:16 +0600

    :construction: oder review right side cart :heavy_check_mark:

--- src/componets/OderReview/OrderReview.jsx
+++ src/componets/OderReview/OrderReview.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import useProducts from '../../Hooks/useProduct';
+import useCart from '../../Hooks/useCart';
+import Cart from '../Cart/Cart';
 
 function OrderReview() {
+    const [products,setProducts] = useProducts();
+    const [cart] = useCart(products);
     return (
-        <div>
-            <h1>This is order review</h1>
+        <div className="shop-container">
+            <div className='product-container'>
+
+            </div>
+
+            <div className='cart-container' >
+                <Cart cart={cart}></Cart>
+
+            </div>
+            
             
         </div>
     )
